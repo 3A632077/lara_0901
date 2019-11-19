4cf9f7bf35689637a7b1c5b0a973fdddf121cbeb
林威震 <weizhen19990727@gmail.com>
2019-11-19 22:08:48 +0800

    3-2 在 View 顯示資料

--- resources/views/admin/posts/create.blade.php
+++ resources/views/admin/posts/create.blade.php
@@ -59,6 +59,18 @@
             <p>&nbsp;</p>
 
         </form>
+        @foreach($posts as $post)
+            <tr>
+                <td>{{$post‐>id}}</td>
+                <td>{{$post‐>title}}</td>
+                <td>{{($post‐>is_feature)?'v':'x'}}</td>
+                <td>
+                    <a href="{{route('admin.posts.edit',$post‐>id)}}">編輯</a>
+                    /
+                    <a href="#">刪除</a>
+                </td>
+            </tr>
+        @endforeach
     </div>
 </div>
 <!-- /.row -->
